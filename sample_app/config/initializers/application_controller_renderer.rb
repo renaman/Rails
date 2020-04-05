5427e3638f53d6c36fbb308a1c47cc2a0494a3dd
renaman <n.renaman@icloud.com>
2020-04-05 22:10:27 +0900

    [Add]3-8画像投稿機能加の作成

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '32031999387e48981b967b452a49e61bff153729974bb013d41adec7ff0e72d5d8162846954d60e971b45ea37174bb16e7f3af6be75ad177a9a9dd31d8c8fe09'
